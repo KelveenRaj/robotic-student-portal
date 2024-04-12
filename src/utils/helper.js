79cddeeacdfea9bc30396e641e6c82b414002868
Kelveen Raj <kelveenrajv@gmail.com>
2024-04-12 22:59:45 +0800

    fix date format

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -16,7 +16,7 @@ const formatDate = (dateString) => {
   const day = date.getDate().toString().padStart(2, "0");
   const month = (date.getMonth() + 1).toString().padStart(2, "0");
   const year = date.getFullYear();
-  return `${month}/${day}/${year}`;
+  return `${day}/${month}/${year}`;
 };
 
 export { jsonParseFromStorage, formatDate };
